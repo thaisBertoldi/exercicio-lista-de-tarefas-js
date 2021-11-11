4343f0ab0b6ff6f86eb57ce97301d23d06db8cc6
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-11 16:05:12 -0300

    Evento criar tarefa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-alert(1);
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+}
+
+btnTarefa.addEventListener('click', function(e) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+})
