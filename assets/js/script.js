719e3401c737807310106377ab2b1ffef1507bf4
Thais Bertoldi <thais_brt@hotmail.com>
2021-11-11 18:19:48 -0300

    Inserido botão de apagar a tarefa

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,14 +19,32 @@ function limpaInput() {
     inputTarefa.focus();
 }
 
+function criaBotaoApagar(li){
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
+    li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput) {
     const li = criaLi();
     li.innerText = textoInput;
     tarefas.appendChild(li);
     limpaInput();
+    criaBotaoApagar(li);
 }
 
 btnTarefa.addEventListener('click', function(e) {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
-})
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
+});
